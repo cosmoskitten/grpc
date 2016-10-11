bda5d834f3f2b1202b3d071b3c5e5695d17aff41
Maciej Lasyk <maciej@lasyk.info>
2016-10-12 00:06:58 +0200

    Updated C_PYTHON_DEV_ERROR_MESSAGE err msg w/RHEL+Fedora
    Added RHEL/Fedora specific information about lacking of python.h - because Linux doesn't mean Ubuntu ;)

--- src/python/grpcio/support.py
+++ src/python/grpcio/support.py
@@ -44,8 +44,10 @@ C_PYTHON_DEV = """
 int main(int argc, char **argv) { return 0; }
 """
 C_PYTHON_DEV_ERROR_MESSAGE = """
-Could not find <Python.h>. This could mean the following:
-  * You're on Ubuntu and haven't `apt-get install`ed `python-dev`.
+Could not find <python.h>. This could mean the following:
+  * You're on Ubuntu and haven't `apt-get install python-dev`.
+  * You're on RHEL/Fedora and haven't `yum/dnf install python-devel`
+    (make sure you also have redhat-rpm-config installed)
   * You're on Mac OS X and the usual Python framework was somehow corrupted
     (check your environment variables or try re-installing?)
   * You're on Windows and your Python installation was somehow corrupted
