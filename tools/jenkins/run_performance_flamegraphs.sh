d4a4359ed96e1f46d2f82a24e4ea83404f6d0a52
Alexander Polcyn <apolcyn@google.com>
2016-12-19 18:28:09 -0800

    add in missing line continuations to not exit early

--- tools/jenkins/run_performance_flamegraphs.sh
+++ tools/jenkins/run_performance_flamegraphs.sh
@@ -39,7 +39,7 @@ tools/run_tests/run_performance_tests.py \
     -l c++ \
     --category scalable \
     --remote_worker_host grpc-performance-server-32core grpc-performance-client-32core grpc-performance-client2-32core \
-    --perf_args "record -F 97 --call-graph dwarf"
+    --perf_args "record -F 97 --call-graph dwarf" \
     || EXIT_CODE=1
 
 # scalability with 32cores go benchmarks
@@ -47,7 +47,7 @@ tools/run_tests/run_performance_tests.py \
     -l go \
     --category scalable \
     --remote_worker_host grpc-performance-server-32core grpc-performance-client-32core grpc-performance-client2-32core \
-    --perf_args "record -F 97 -g"
+    --perf_args "record -F 97 -g" \
     || EXIT_CODE=1
 
 exit $EXIT_CODE
